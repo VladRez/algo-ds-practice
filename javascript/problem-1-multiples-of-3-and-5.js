03c66cde2dc39ff1b8c79ca3256aa92d47df9aa0
Vladislav Reznikov <vladrezdev@gmail.com>
2018-09-09 16:08:16 -0700

    added project euler solutions

---
+++ javascript/problem-1-multiples-of-3-and-5.js
@@ -0,0 +1,26 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+
+var multiplesSum = function(userValue){
+	var result = 0;
+
+	for (i = 0; i < userValue; i++){
+		if (i % 3 == 0 || i % 5 == 0){
+		result += i;
+	
+		}
+	}
+	return result;
+}
+
+
+console.log(multiplesSum(1000));
+
+
+
+
+
