aea1fb3ddbe0a778c072ec20cfca7e97fae1a4c4
Vlad Rez <vladrezdev@gmail.com>
2018-09-20 17:32:06 -0700

    add reverse string common challange in js

---
+++ common-challanges/JavaScript/reverse-string.js
@@ -0,0 +1,27 @@
+// reverse a string
+
+//standard approach
+function reverseStr(str){
+var reversedStr = '';
+//var str = str.split(''); can also be used
+
+	for (var i = str.length - 1 ; i >= 0; i--){
+		reversedStr += str[i];
+	}
+
+	return reversedStr;
+}
+
+
+//using js methods
+function reverseStrByMethods(str){
+	return str.split('').reverse().join('');
+	
+}
+
+
+var testStr = "The quick brown fox jumped over the lazy dog."
+
+console.log(reverseStr(testStr));
+console.log(reverseStrByMethods(testStr));
+
