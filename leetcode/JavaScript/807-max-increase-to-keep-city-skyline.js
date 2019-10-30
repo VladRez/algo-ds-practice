b3a6d3a0bf3b87d55ade332e8ae4a16b35f35813
VladRez <vladrezdev@gmail.com>
2019-10-30 12:13:43 -0700

    add leetcode 807 alternative solution

--- leetcode/JavaScript/807-max-increase-to-keep-city-skyline.js
+++ leetcode/JavaScript/807-max-increase-to-keep-city-skyline.js
@@ -1,8 +1,9 @@
 var maxIncreaseKeepingSkyline = function(grid) {
     let increase = 0;
     for(let row = 0; row < grid.length; row++){
+        let rowMax = Math.max(...grid[row])
         for(let col = 0; col < grid[row].length; col++){
-          let rowMax = Math.max(...grid[row])
+          
           let colMax = getColMax(grid, col);
           let max = rowMax > colMax ?  colMax : rowMax
           increase += max - grid[row][col]      
@@ -18,3 +19,29 @@ function getColMax(grid, col){
     }
     return Math.max(...colArr)
 }
+
+
+/// better
+
+var maxIncreaseKeepingSkyline = function(grid) {
+    let increase = 0;
+    let transposed = [];
+     for(let row = 0; row < grid.length; row++){
+         transposed.push([]);
+     };
+     for(let row = 0; row < grid.length; row++){
+         for(let col = 0; col < grid.length; col++){
+             transposed[col].push(grid[row][col]);
+         };
+     };
+     
+     for(let row = 0; row < grid.length; row++){
+             let rowMax = Math.max(...grid[row]);
+         for(let col = 0; col < grid[row].length; col++){
+             let colMax = Math.max(...transposed[col])
+             let max = rowMax > colMax ? colMax : rowMax
+             increase += max - grid[row][col]
+         };
+     };
+     return increase;
+ };
